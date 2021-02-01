f0fc336febeacd295dfc0acb1d6f3e12116d3aa5
Eduard-al <75543603+Eduard-al@users.noreply.github.com>
2021-02-01 21:10:39 +0300

    Update gameCode.js

--- lessom4/millioner/gameCode.js
+++ lessom4/millioner/gameCode.js
@@ -16,17 +16,10 @@ function box() {
             ok = isAnswer(answerQuantity, event);
         }
 
-
-
-
-        //console.log(answers);
     } while (!ok);
 }
 box()
 
-
-
-
 switch (event) {
     case 1:
     case 2:
@@ -118,73 +111,6 @@ switch (event) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* switch (event) {
-    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
-        mas = [works.b00, works.b1, works.b2];
-        answerQuantity = works.b0;
- 
- 
-        box()
-        switch (event) {
-            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
-                mas = [works.d00, works.d1, works.d2];
-                answerQuantity = works.d0;
- 
-                box()
-                break;
-            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
-                mas = [works.d00, works.d1, works.d2];
-                answerQuantity = works.d0;
- 
-                box()
-                break;
-            case -1: // Второе действие
-                break;
-            default:
-                alert('Ошибка');
-        }
-        break;
-    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
-        mas = [works.c00, works.c1, works.c2];
-        answerQuantity = works.c0;
- 
-        box()
-        switch (event) {
-            case 1: // Второе действие
-                mas = [works.d00, works.d1, works.d2];
-                answerQuantity = works.d0;
- 
-                box()
-                break;
-            case 2: // Второе действие
-                mas = [works.d00, works.d1, works.d2];
-                answerQuantity = works.d0;
- 
-                box()
-                break;
-            case -1: // Второе действие
-                break;
-            default:
-                alert('Ошибка');
-        }
-        break;
-    case -1: // Первое действие
-        break;
-    default:
-        alert('Ошибка');
-} */
 alert('вы правильно ответили на ' + i + ' вопросов\n')
 alert('Спасибо за игру');
 
